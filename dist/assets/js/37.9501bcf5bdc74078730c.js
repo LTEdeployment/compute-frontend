b138dd007d65c1b4b9a8b3b6d268144531449b50
XhinLiang <xhinliang@gmail.com>
2017-03-05 22:43:24 +0800

    add validator in Create.vue, add Xinput component.

---
+++ dist/assets/js/37.9501bcf5bdc74078730c.js
@@ -0,0 +1,2 @@
+webpackJsonp([37,45],{121:function(e,t,n){var r=n(0)(n(387),n(609),null,null);e.exports=r.exports},387:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},609:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("router-view")],1)},staticRenderFns:[]}}});
+//# sourceMappingURL=37.9501bcf5bdc74078730c.js.map
---
+++ dist/assets/js/41.cc77573f79bc8e8eca98.js
@@ -0,0 +1,2 @@
+webpackJsonp([41,45],{366:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={}},559:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("router-view")],1)},staticRenderFns:[]}},99:function(e,t,n){var r=n(0)(n(366),n(559),null,null);e.exports=r.exports}});
+//# sourceMappingURL=41.cc77573f79bc8e8eca98.js.map
